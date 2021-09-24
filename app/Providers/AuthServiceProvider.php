b5097b05287441d0e9a671dd25b074b7aed0b09d
Bianca <biaformic@hotmail.com>
2021-09-24 12:34:27 -0300

    Adding load keys

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,5 +26,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
     }
 }
